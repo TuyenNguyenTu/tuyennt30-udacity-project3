84352300983bf2642341f5e872d5a0472c954293
KevinNguyen <nguyentutuyen45@gmail.com>
2024-10-12 18:41:19 +0700

    3rd commit

---
+++ .bin/create-cluster.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+eksctl create cluster --name tuyennt30-project3-cluster --region=us-east-1 --nodes-min=2 --nodes-max=3
--- .bin/eks_config.sh
+++ .bin/eks_config.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-aws eks create cluster --name tuyennt30-project3-cluster --region=us-east-1 --nodes-min=2 --nodes-max=3
 aws eks --region us-east-1 update-kubeconfig --name tuyennt30-project3-cluster
